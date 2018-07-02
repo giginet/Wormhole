8570f9d90ab892ab7254a03bd7477f78a3c6cf33
giginet <giginet.net@gmail.com>
2018-07-03 00:46:28 +0900

    Add Result to Package.swift

--- Package.swift
+++ Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["Wormhole"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/antitypical/Result.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Wormhole",
-            dependencies: []),
+            dependencies: ["Result"]),
         .testTarget(
             name: "WormholeTests",
             dependencies: ["Wormhole"]),
